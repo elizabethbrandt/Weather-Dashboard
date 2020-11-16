920db7b2b438dbb03fc91b338cd0721efe7cfa6c
Elizabeth Brandt <ebrandt8@uw.edu>
2020-11-15 16:12:53 -0800

    working on 5 day forecast function and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,10 @@
 var today = moment().format('(MM/DD/YYYY)');
 
 // Add event listener to the search button
-// $('#button-addon2').on("click", "button", searchOWM);
+$('#button-addon2').on("click", "button", currentConditions);
 
 // The search OWM function takes a cityName, searches the OWM api for it, and then passes the data to
-var searchOWM = function (event) {
+function currentConditions() {
 
     // event.preventDefault();
 
@@ -25,28 +25,53 @@ var searchOWM = function (event) {
 
     // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appikey=35f5097b02a181982a5bb6c4eee0ce65";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=166a433c57516f51dfab1f7edaed8413"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=166a433c57516f51dfab1f7edaed8413&units=imperial"
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-            console.log(response);
-            $('#city-name').text(JSON.stringify(response.name + ' ' + today));
-            $('#temperature').text(JSON.stringify('Temperature: ' + response.main.temp));
-            $('#humidity').text(JSON.stringify('Humidity: ' + response.main.humidity + '%'));
-            $('#wind-speed').text(JSON.stringify('Wind Speed: ' + response.wind.speed + ' MPH'));
-            // $('#uv-index').text(JSON.stringify(response.));
-            // console.log(JSON.parse(response.name));
+            // console.log(response);
+            $('#city-name').text(response.name + ' ' + today);
+            var icon = response.weather[0].icon
+            $('#icon').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png');
+            $('#temperature').text('Temperature: ' + response.main.temp + ' °F');
+            $('#humidity').text('Humidity: ' + response.main.humidity + '%');
+            $('#wind-speed').text('Wind Speed: ' + response.wind.speed + ' MPH');
+            // $('#uv-index').text(response.);
         });
 };
-searchOWM();
-// searchOWM("userInput")
+currentConditions();
+// currentConditions("userInput")
 
 // Pull in current weather info to the main card
 
 // Pull in 5-day forecast to the weather cards below
+function forecast() {
 
+    var cityName = $('#city-input').val().trim();
+
+    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appikey=35f5097b02a181982a5bb6c4eee0ce65";
+
+    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Bujumbura,Burundi&appid=166a433c57516f51dfab1f7edaed8413&units=imperial"
+
+    $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+
+            for (var i = 0; i < 5; i++)
+                // $('#humidity').text('Humidity: ' + response.main.humidity + '%');
+                // var icon = response.weather[0].icon
+                // $('#icon').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png');
+                // $('#temperature').text('Temperature: ' + response.main.temp + ' °F');
+                // $('#humidity').text('Humidity: ' + response.main.humidity + '%');
+        });
+
+}
+forecast();
 // MIGHT need something like this for the search history??
 // $("#buttons-view").empty();
